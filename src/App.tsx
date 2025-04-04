29d6df173dadef6b55ba09ce59b65843de5e5028
veshon <vashonmabima12@gmail.com>
2025-04-04 09:41:28 +0530

    Delete user

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_BASE_URL = "https://8015b5dbc0724d38882ac90397c27649.weavy.io";
+const API_URL = "https://8015b5dbc0724d38882ac90397c27649.weavy.io";
 const API_TOKEN = "wys_hMWpXdekxcn9Gc8Ioah3azOllzUZ7l3HN9yB";
 
 const App: React.FC = () => {
@@ -15,7 +15,7 @@ const App: React.FC = () => {
 
   const fetchUsers = async () => {
     try {
-      const response = await axios.get(API_BASE_URL, {
+      const response = await axios.get(API_URL, {
         headers: { Authorization: `Bearer ${API_TOKEN}` },
       });
       setUsers(response.data);
@@ -26,7 +26,7 @@ const App: React.FC = () => {
 
   const createUser = async () => {
     try {
-      const response = await axios.post(API_BASE_URL, userData, {
+      const response = await axios.post(API_URL, userData, {
         headers: { Authorization: `Bearer ${API_TOKEN}` },
       });
       setUsers([...users, response.data]);
@@ -37,7 +37,7 @@ const App: React.FC = () => {
 
   const deleteUser = async (userId: number) => {
     try {
-      await axios.delete(`${API_BASE_URL}/${userId}`, {
+      await axios.delete(`${API_URL}/${userId}`, {
         headers: { Authorization: `Bearer ${API_TOKEN}` },
       });
       setUsers(users.filter((user) => user.id !== userId));
